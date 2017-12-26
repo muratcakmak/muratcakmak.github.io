682d227acef8b826f8ff77c2fa5826a8641b1ad0
Oguzhan Cakmak <oguzhanmc@gmail.com>
2017-12-26 01:46:32 -0500

    Updates

--- path---index-0044fcf94d30884a3052.js
+++ path---index-0044fcf94d30884a3052.js
@@ -1,2 +1,2 @@
-webpackJsonp([0x81b8806e4260],{349:function(t,a){t.exports={data:{site:{siteMetadata:{title:"Oguzhan Cakmak"}}},pathContext:{}}}});
+webpackJsonp([0x81b8806e4260],{350:function(t,a){t.exports={data:{site:{siteMetadata:{title:"Oguzhan Cakmak"}}},pathContext:{}}}});
 //# sourceMappingURL=path---index-0044fcf94d30884a3052.js.map
